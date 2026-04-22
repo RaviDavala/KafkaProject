02775e79162b350b607890fc73ad90c3d7a3714e
RaviDavala <ravirockstar10990@gmail.com>
2026-04-22 12:23:29 +0000

    First_Push
    changes involving bronze and silver layer for both dimension and the events data from kafka topics.

---
+++ Kafka OTT Stream/transformations/Kafka Ingestion.py
@@ -0,0 +1,88 @@
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
+import dlt
+
+spark.conf.set("spark.sql.session.timeZone", "UTC")
+
+bootstrap = spark.conf.get("bootstrap_server")
+api_key = spark.conf.get("kafka_api_key")
+api_secret = spark.conf.get("kafka_secret")
+events_topic = spark.conf.get("kafka_topic_events")
+dim_topic = spark.conf.get("kafka_topic_dim")
+catalog_name = spark.conf.get("catalog_name")
+bronze_schema = spark.conf.get("bronze_schema")
+
+@dlt.table(
+    name=f"{catalog_name}.{bronze_schema}.watch_events_bronzeV2"
+)
+def kafka_bronze_events():
+
+    df_events = spark.readStream \
+        .format("kafka") \
+        .option("kafka.bootstrap.servers", bootstrap) \
+        .option("subscribe", events_topic) \
+        .option("startingOffsets", "earliest") \
+        .option("kafka.security.protocol", "SASL_SSL") \
+        .option("kafka.sasl.mechanism", "PLAIN") \
+        .option(
+            "kafka.sasl.jaas.config",
+            f"kafkashaded.org.apache.kafka.common.security.plain.PlainLoginModule "
+            f"required username='{api_key}' password='{api_secret}';"
+        ) \
+        .load()
+
+    df = df_events.selectExpr("CAST(value AS STRING)")
+
+    schema = StructType([
+        StructField("user_id", StringType()),
+        StructField("content_id", StringType()),
+        StructField("title", StringType()),
+        StructField("watch_time_sec", DoubleType()),
+        StructField("event_type", StringType()),
+        StructField("device", StringType()),
+        StructField("event_time", DoubleType())
+    ])
+
+    df = df.withColumn("json", from_json(col("value"), schema)).select("json.*")
+
+    df = df.withColumn(
+        "event_time",
+        expr("timestamp_seconds(event_time)")
+    )
+
+    return df.withColumn("ingested_at", current_timestamp())
+
+
+@dlt.table(
+    name = f"{catalog_name}.{bronze_schema}.content_dim_bronze"
+)
+def kafka_bronze_content_dim():
+    df_raw = spark.readStream \
+        .format("kafka") \
+        .option("kafka.bootstrap.servers", bootstrap) \
+        .option("subscribe", dim_topic) \
+        .option("startingOffsets", "earliest") \
+        .option("kafka.security.protocol", "SASL_SSL") \
+        .option("kafka.sasl.mechanism", "PLAIN") \
+        .option(
+            "kafka.sasl.jaas.config",
+            f"kafkashaded.org.apache.kafka.common.security.plain.PlainLoginModule "
+            f"required username='{api_key}' password='{api_secret}';"
+        ) \
+        .load()
+
+    df = df_raw.select(col("value").cast("String"))
+
+    schema = StructType([
+        StructField("content_id", StringType()),
+        StructField("title", StringType()),
+        StructField("genre", StringType()),
+        StructField("duration_sec", DoubleType()),
+        StructField("release_year", StringType())
+    ])
+
+    df = df.withColumn("json", from_json(col("value"), schema))
+
+    df = df.select("json.*")
+
+    return df.withColumn("ingested_at", current_timestamp())
---
+++ Kafka OTT Stream/transformations/watch_events_transform.py
@@ -0,0 +1,75 @@
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
+import dlt
+
+silver_schema = spark.conf.get("silver_schema")
+bronze_schema = spark.conf.get("bronze_schema")
+catalog_name = spark.conf.get("catalog_name")
+
+@dlt.table(
+    name=f"{catalog_name}.{silver_schema}.watch_events_quarantineV2"
+)
+def watch_events_quarantine():
+
+    df = dlt.read_stream(f"{catalog_name}.{bronze_schema}.watch_events_bronzeV2")
+
+    return df.withColumn(
+        "error_reason",
+        when(col("content_id").isNull(), "Missing content_id")
+        .when(col("user_id").isNull(), "Missing user_id")
+        .when(col("title").isNull(), "Missing title")
+        .when(col("watch_time_sec").isNull(), "Missing watch_time_sec")
+        .when(col("event_time").isNull(), "Missing event_time")
+        .when(col("event_type").isNull(), "Missing event_type")
+        .when(col("watch_time_sec") <= 0, "Negative watch_time")
+    ).filter(col("error_reason").isNotNull()) \
+     .withColumn("quarantine_time", current_timestamp())
+
+
+@dlt.table(
+    name=f"{catalog_name}.{silver_schema}.watch_events_silverV2"
+)
+@dlt.expect_or_drop(
+    "valid_not_null",
+    "content_id IS NOT NULL AND user_id IS NOT NULL AND title IS NOT NULL AND watch_time_sec IS NOT NULL AND event_time IS NOT NULL AND event_type IS NOT NULL"
+)
+@dlt.expect_or_drop(
+    "valid_watch_time",
+    "watch_time_sec > 0 AND watch_time_sec <= 14400"
+)
+def watch_events_silver():
+
+    df = dlt.read_stream(f"{catalog_name}.{bronze_schema}.watch_events_bronzeV2")
+
+    for c in ["title", "event_type", "device"]:
+        df = df.withColumn(c, trim(lower(col(c))))
+
+    for c in ["content_id", "user_id"]:
+        df = df.withColumn(c, trim(col(c)))
+
+    df = df.withColumn(
+        "device",
+        when(col("device").rlike("mobile|android|iphone"), "Mobile")
+        .when(col("device").rlike("tv|smarttv"), "TV")
+        .when(col("device").rlike("windows|mac|linux"), "PC")
+        .when(col("device").rlike("ipad|tablet"), "Tablet")
+        .when(col("device").rlike("web"), "Internet")
+        .otherwise("Unknown")
+    ).fillna("Unknown", ["device"])
+
+    valid_events = ["play", "pause", "stop"]
+
+    df = df.withColumn(
+        "event_type",
+        when(col("event_type").isin(valid_events), initcap(col("event_type")))
+        .otherwise("Unknown")
+    )
+
+    df = df.withWatermark("event_time", "10 minutes") \
+        .dropDuplicates(["user_id", "content_id", "event_time", "watch_time_sec"])
+
+    df = df.withColumn("watch_time_minutes", col("watch_time_sec") / 60) \
+           .withColumn("event_date", to_date(col("event_time"))) \
+           .withColumn("event_hour", hour(col("event_time")))
+
+    return df.withColumn("validated_at", current_timestamp())
